2962e90889f5ad5f1921d5c7d9eaa38fb62edc6d
Amos Ngoah <ngoahamos@gmail.com>
2019-04-16 10:19:42 +0000

    first commit

---
+++ src/app/routes/users/new-user/new-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styles: []
+})
+export class NewUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
